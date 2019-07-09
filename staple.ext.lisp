4f47c47af1360689931609a39a69db39ab42ec4f
Shinmera <shinmera@tymoon.eu>
2019-07-09 17:13:13 +0200

    Fix docs.

--- staple.ext.lisp
+++ staple.ext.lisp
@@ -4,8 +4,16 @@
   (:use #:cl)
   (:local-nicknames (#:sequences #:org.shirakumo.trivial-extensible-sequences)))
 
+(defclass simple-page* (staple:simple-page) ())
+
+(defmethod staple:document-package ((page simple-page*))
+  (find-package '#:org.shirakumo.trivial-extensible-sequences.doc))
+
 (defmethod staple:packages ((_ (eql (asdf:find-system :trivial-extensible-sequences))))
-  (list (find-package '#:org.shirakumo.trivial-extensible-sequences.doc)))
+  (list (find-package '#:org.shirakumo.trivial-extensible-sequences)))
+
+(defmethod staple:page-type ((_ (eql (asdf:find-system :trivial-extensible-sequences))))
+  'simple-page*)
 
 #+sbcl
 (defmethod staple:definition-wanted-p ((_ definitions:source-transform) page) NIL)
